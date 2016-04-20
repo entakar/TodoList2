d89fe16444e77f437ccbec3a4c2153f50298b523
EndoTakashi <TakEnd@endotakashi-no-macbook-pro-2.local>
2016-04-20 23:34:52 +0900

    Initial Commit

---
+++ TodoList2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList2
+//
+//  Created by EndoTakashi on 2016/04/20.
+//  Copyright © 2016年 tak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
